9f8823c4d72d31aeca3fd59a8998367cc9afeb2a
Chad Edwards <chadedwardsofficial@gmail.com>
2023-09-19 17:39:38 -0500

    declared a password length variable and gave the user a prompt to put in a number between 8 and 128, user will be given an error message if the prompted value is less than 8 and greater than 128

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   alert("A successful password should include uppercase & lowercase letters, number(s) and a special character");
 
-  
+  var passwordlength = parseInt(prompt("Please choose a password length (between 8 and 128"));
+  if (passwordlength < 8 || passwordlength > 128) {
+    alert("Please enter a numbered length for your password between 8 and 128 please.")
+    return;
+  } 
 
 }
 
