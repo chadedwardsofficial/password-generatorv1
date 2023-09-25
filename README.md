23f2f2ff74780530d7083f7bd8b30a98bdd19ef8
Chad Edwards <chadedwardsofficial@gmail.com>
2023-09-25 15:12:36 -0500

    added a screenshot and a credit/readme/update section to readme file using markdown language

---
+++ README.md
@@ -0,0 +1,36 @@
+## Password Generator V1
+ 
+This website is a randomized password generator. This websites goal to accomplish was to prompt the user to answer a series of questions with criteria for the password to be generated upon.
+Questions asked varied from "How many character length do you want your password to be" to "Would you like to include uppercase characters?". These questions helped the generator make a password
+based on the criteria the user prompted. A user can create a password with only Numbers, only lowercase chars, only uppercase and only special chars. A user can have any variation of these including all of them together.
+I hope this website helps to best serve anyone who is having difficulty creating their own password. 
+
+
+## Link to Website:
+
+
+
+## Screenshot of Working Website: 
+
+![Alt text](image.png)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Credit:
+All techniques used in this website are derived from SMU Coding Boot Camp, Online Internet Resources and reddit.
+
+
+
+## Update:
+Last Updated 9/25/2023
--- script.js
+++ script.js
@@ -8,8 +8,8 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   alert("A successful password should include uppercase & lowercase letters, number(s) and a special character.");
 
-  // Setting a variable to what the user is prompted to put in. Including parseInt to conver it to an integer. //
-  // If user puts in a numeric value that is less than 8 or more than 128, it will be prompted with an alert error message //
+    // Setting a variable to what the user is prompted to put in. Including parseInt to convert it to an integer. //
+    // If user puts in a numeric value that is less than 8 or more than 128, it will be prompted with an alert error message //
 
   var passwordlength = parseInt(prompt("Please choose a password length (between 8 and 128)"));
 if (passwordlength < 8 || passwordlength > 128) {
@@ -36,8 +36,8 @@ if (!usedLowercase && !usedUppercase && !usedNumbers  && !usedSpecialChars){
   var numberChars = '0123456789';
   var specialChars = '!@#$%^&*()_+[]{}|;:,.<>?';
 
-// This line of code declares the variable 'allChars' to be empty so that we can add all 4 above variables together based on the used criteria//
-// If the prompts are boolean true or false, it will then add the set of characters to the allChars set and then run it's next line of code//
+      // This line of code declares the variable 'allChars' to be empty so that we can add all 4 above variables together based on the used criteria//
+      // If the prompts are boolean true or false, it will then add the set of characters to the allChars set and then run it's next line of code//
 
     var allChars = ''
   if (usedLowercase) allChars += lowercaseChars;
@@ -45,12 +45,12 @@ if (!usedLowercase && !usedUppercase && !usedNumbers  && !usedSpecialChars){
   if (usedNumbers) allChars += numberChars;
   if (usedSpecialChars) allChars += specialChars;
 
-  // This block of code now declares a variable(generatedPassword) with an empty value//
-  // We run a for loop algorithm to prompt the computer to run this until 'i' is greater than the passwordLength chosen by the user on line 9//
-  // Inside the for loop we declare a variable (randomIndex) to be our indexed number that has a randomized generator using Math.random. //
-  // We multiply the randomized numeric value from math.random onto the length of allChars.length (the length of the array of the value allChars) //
-  // The randomized number will be at first set to a decimal int. We use the Math.floor method to round the number to its nearest whole integer//
-  // Finally we take that randomly generated number assigned to randomIndex and put that as the index that will be chosen within allChars[]//
+      // This block of code now declares a variable(generatedPassword) with an empty value//
+      // We run a for loop algorithm to prompt the computer to run this until 'i' is greater than the passwordLength chosen by the user on line 9//
+      // Inside the for loop we declare a variable (randomIndex) to be our indexed number that has a randomized generator using Math.random. //
+      // We multiply the randomized numeric value from math.random onto the length of allChars.length (the length of the array of the value allChars) //
+      // The randomized number will be at first set to a decimal int. We use the Math.floor method to round the number to its nearest whole integer//
+      // Finally we take that randomly generated number assigned to randomIndex and put that as the index that will be chosen within allChars[]//
 
   var generatedPassword = '';
 
@@ -60,8 +60,8 @@ if (!usedLowercase && !usedUppercase && !usedNumbers  && !usedSpecialChars){
 
   }
 
-  // Now it returns the value of the variable (generatedPassword) //
-  // It will continue to add the randomized variable onto itself until the code ends which is when i < password.length //
+      // Now it returns the value of the variable (generatedPassword) //
+      // It will continue to add the randomized variable onto itself until the code ends which is when i < password.length //
 
 return generatedPassword;
 
